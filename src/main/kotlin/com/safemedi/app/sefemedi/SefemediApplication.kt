114b66d4daed9bb3ca22493e7b5462482dd8bbfe
jung <agppg@naver.com>
2026-05-02 03:56:07 +0900

    feat: Initialize SafeMedi project with Spring Boot and Domain structure

---
+++ src/main/kotlin/com/safemedi/app/sefemedi/SefemediApplication.kt
@@ -0,0 +1,11 @@
+package com.safemedi.app.sefemedi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SefemediApplication
+
+fun main(args: Array<String>) {
+	runApplication<SefemediApplication>(*args)
+}

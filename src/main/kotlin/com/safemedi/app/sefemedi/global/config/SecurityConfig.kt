e96ee8da21c261f3479988a9cb0bbe3e476bc915
jung <agppg@naver.com>
2026-06-22 02:49:09 +0900

    [SAF-78] chore: 모니터링 설정 수정

--- src/main/kotlin/com/safemedi/app/sefemedi/global/config/SecurityConfig.kt
+++ src/main/kotlin/com/safemedi/app/sefemedi/global/config/SecurityConfig.kt
@@ -44,7 +44,6 @@ class SecurityConfig(
                     "/v3/api-docs",
                     "/v3/api-docs/**",
                     "/actuator/health",
-                    "/actuator/prometheus",
                 ).permitAll()
 
                 it.requestMatchers(

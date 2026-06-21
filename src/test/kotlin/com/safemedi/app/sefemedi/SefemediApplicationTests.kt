f60e0d0a595f3ad3581873e2558070563cdfcbd7
kimminho2004 <ho28069534@gmail.com>
2026-06-22 00:52:10 +0900

    fix: test profile 추가로 ApplicationContext 로딩 경고 해소

--- src/test/kotlin/com/safemedi/app/sefemedi/SefemediApplicationTests.kt
+++ src/test/kotlin/com/safemedi/app/sefemedi/SefemediApplicationTests.kt
@@ -3,21 +3,13 @@ package com.safemedi.app.sefemedi
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Primary
 import tools.jackson.databind.json.JsonMapper
 
-@SpringBootTest(
-	properties = [
-		"spring.datasource.url=jdbc:h2:mem:safemedi-test;MODE=MySQL;DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false",
-		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.datasource.username=sa",
-		"spring.datasource.password=",
-		"spring.jpa.hibernate.ddl-auto=none",
-		"spring.flyway.enabled=false",
-		"jwt.secret=abcdefghijklmnopqrstuvwxyz123456",
-	]
-)
+@SpringBootTest
+@ActiveProfiles("test")
 class SefemediApplicationTests {
 
 	@Test

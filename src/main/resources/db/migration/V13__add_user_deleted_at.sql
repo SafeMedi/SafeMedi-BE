37e71db39c63c32b07c3958c6f9be54a0a02e528
kimminho2004 <ho28069534@gmail.com>
2026-07-06 18:52:39 +0900

    fix(db): user.deleted_at 컬럼을 datetime(6)으로 변경

--- src/main/resources/db/migration/V13__add_user_deleted_at.sql
+++ src/main/resources/db/migration/V13__add_user_deleted_at.sql
@@ -1,4 +1,4 @@
 -- dialect: MySQL
 -- noinspection SqlDialectInspection
 ALTER TABLE `user`
-    ADD COLUMN `deleted_at` datetime NULL COMMENT '탈퇴 처리 일시' AFTER `updated_at`;
+    ADD COLUMN `deleted_at` datetime(6) NULL COMMENT '탈퇴 처리 일시' AFTER `updated_at`;
